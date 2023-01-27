0048c6ed79bd75357de1fc28eed6593b519b10ce
McKenzie <mckenzie.nalley@gmail.com>
2023-01-27 13:03:43 -0500

    Added password match verfication.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# form
+# Form
 A dummy sign-up form to practice front-end design.
+
+The form has front-end verifcation that ensures the email and phone number are formatted correctly, and a dynamic check and help message to ensure that the password and password confirmation match.
+
+The page also has a light and dark theme, and is responsive for mobile and desktop.
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Theme toggle
+
 document.documentElement.className = "light";
 
 const themeToggle = document.querySelector('#theme-toggle');
@@ -11,3 +13,34 @@ function setTheme(e) {
     themeToggle.textContent = themeToggle.textContent === 'Dark' ? 'Light' : 'Dark'; 
 }
 
+// Password confirmation
+
+const passwords =document.querySelectorAll('.password');
+
+const passwordInput = document.querySelector('#password');
+const passwordConfirmInput = document.querySelector('#password-confirm');
+
+
+passwords.forEach(el => el.addEventListener('input', checkPasswords));
+
+function checkPasswords(e) {
+    if (passwordInput.value === '' || passwordConfirmInput.value === '')
+    {
+        return;
+    }
+    else if (passwordInput.value === passwordConfirmInput.value)
+    {
+        passwords.forEach(el => el.setCustomValidity(''));
+        passwords.forEach(el => {
+            el.classList.remove('error');
+            el.parentElement.classList.remove('error');
+        });
+
+    } else {
+        passwords.forEach(el => el.setCustomValidity('Passwords must match.'));
+        passwords.forEach(el => {
+            el.classList.add('error');
+            el.parentElement.classList.add('error');
+        });
+    }
+}
